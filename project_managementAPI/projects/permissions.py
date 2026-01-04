42ae688641fe3d48c771efb9ba1b671896a42f5f
Kevin.Ogutu <kogutu7@gmail.com>
2026-01-04 15:41:05 +0300

    Projects APP Permissions Update

--- project_managementAPI/projects/permissions.py
+++ project_managementAPI/projects/permissions.py
@@ -1,16 +1,13 @@
 from rest_framework import permissions
 
 class IsProjectManagerOrReadOnly(permissions.BasePermission):
-    """
-    Allow full write for project manager, read-only for others.
-    For status-change endpoint we will require manager specifically.
-    """
+    
 
     def has_object_permission(self, request, view, obj):
-        # SAFE methods allowed
+       
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Allow manager (object.manager) to edit
+       
         return request.user.is_authenticated and obj.manager == request.user and request.user.is_project_manager()
 class IsProjectManager(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
